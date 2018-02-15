a3c0633ac88f182736e771c10a38cc49c687d7ff
falcon-it <ilya.sokolov@live.ru>
2018-02-15 16:51:21 +0200

    +

--- src/packet/PacketIDTest.java
+++ src/packet/PacketIDTest.java
@@ -2,22 +2,40 @@ package packet;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 import org.junit.Test;
 
 import packet.Packet.DuplicateKeyException;
+import packet.Registry.DynamicIDTypeArrayException;
+import packet.Registry.IsMultiLevelArrayException;
 
 public class PacketIDTest {
 
 	@Test
-	public void test() throws DuplicateKeyException {
+	public void test() throws DuplicateKeyException, NullPointerException, IsMultiLevelArrayException, DynamicIDTypeArrayException {
+		LinkedList<Integer> ids = new LinkedList<>();
 		Packet p = new Packet();
+		ids.add(p.calculateDynamicID());
 		p.add("k5", "ortjskhopgtrk");
-		p.add("k1", 788);
-		p.add("k2", 788.7878);
-		p.add("k4", "gioerhsgirjeiopgjeriop");
-		p.add("k3", false);
 		int id = p.calculateDynamicID();
-		fail("Not yet implemented");
+		assertFalse(ids.contains(id));
+		ids.add(p.calculateDynamicID());
+		p.add("k1", 788);
+		id = p.calculateDynamicID();
+		assertFalse(ids.contains(id));
+		ids.add(p.calculateDynamicID());
+		p.add("k2", 788.7878);
+		id = p.calculateDynamicID();
+		assertFalse(ids.contains(id));
+		ids.add(p.calculateDynamicID());
+		p.add("k4", "gioerhsgirjeiopgjeriop");
+		id = p.calculateDynamicID();
+		assertFalse(ids.contains(id));
+		ids.add(p.calculateDynamicID());
+		p.add("k3", false);
+		id = p.calculateDynamicID();
+		assertFalse(ids.contains(id));
 	}
 
 }
